3dbf3dc35be402f9267b728eca112d173d8bac1b
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 12:51:02 +1000

    Update 01_03_printmenus_ch.sql

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,8 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT Name, Price from Dishes order by Price
 -- Create a report showing appetizers and beverages.
+Select Name, Price FROM Dishes where Type in ('Appetizer','Beverage') order by Type
 -- Create a report with all items except beverages.
+Select Name, Price FROM Dishes where Type != 'Beverage' ORDER BY Type
