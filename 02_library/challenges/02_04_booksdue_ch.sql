9753fefe225a1bc8f3eb6ffc40f5fd28a16235e5
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 15:19:41 +1000

    Update 02_04_booksdue_ch.sql

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,17 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Patrons on Patrons.PatronID = Loans.PatronID
+JOIN Books on Books.BookID = Loans.BookID
+WHERE Loans.DueDate = '2022-07-13'
+AND Loans.ReturnedDate IS NULL;
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Patrons on Patrons.PatronID = Loans.PatronID
+JOIN Books on Books.BookID = Loans.BookID
+WHERE Loans.ReturnedDate IS NULL
+ORDER BY DueDate ASC;
