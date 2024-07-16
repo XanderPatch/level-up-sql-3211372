5ac33f891c1660a7b2569855b604ffb22cd2a016
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 14:44:55 +1000

    Update 02_01_checkavailability_ch.sql

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,22 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT Count(Title) AS CopiesOwned
+FROM Books
+WHERE Title = 'Dracula'
+GROUP BY Title;
+
+SELECT count(*) AS CopiesLoaned
+FROM Loans
+WHERE BookID IN (SELECT BookID FROM Books WHERE Title = 'Dracula')
+and ReturnedDate IS NULL;
+
+SELECT
+(SELECT Count(Title)
+FROM Books
+WHERE Title = 'Dracula'
+GROUP BY Title)-
+(SELECT count(*) 
+FROM Loans
+WHERE BookID IN (SELECT BookID FROM Books WHERE Title = 'Dracula')
+and ReturnedDate IS NULL) AS CopiesAvailable;
