6a11e18149fc5099e197098cfe09ff5f2fade809
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 13:37:51 +1000

    Update 01_08_findreservation_ch.sql

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,8 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT FirstName, LastName, Date, PartySize FROM Customers 
+JOIN Reservations on Customers.CustomerID = Reservations.CustomerID
+WHERE LastName LIKE 'Ste%n'
+AND Date LIKE '%06-14%';
