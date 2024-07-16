bd534f0e17d8045e3fc74b322434e4e20a7a2eea
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 15:34:05 +1000

    Update 02_06_visitreminder_ch.sql

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,8 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT Patrons.FirstName,Patrons.Email, count(Loans.BookID) AS LoanedTimes from Loans
+Join Patrons on Loans.PatronID = Patrons.PatronID
+GROUP by Loans.PatronID
+Order by LoanedTimes ASC
+LIMIT 9;
