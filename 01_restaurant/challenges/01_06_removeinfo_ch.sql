db3302d575d554e37ff0a4a8ebbb2d45ad7a06cb
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 13:22:26 +1000

    Update 01_06_removeinfo_ch.sql

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,14 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT * FROM Customers WHERE FirstName = 'Norby';
+--Norby's CustomerID = 64
+
+SELECT * 
+FROM Reservations 
+WHERE CustomerID = 64
+AND Date > '2022-07-24';
+--ReservationID = 2000
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
