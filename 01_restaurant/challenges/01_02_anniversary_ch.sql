4ebfe57b0735070a8a04fd58b96a434579d8dfc2
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 12:50:35 +1000

    Update 01_02_anniversary_ch.sql

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -3,5 +3,7 @@
 
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
-
+CREATE TABLE Party(
+Customer INTEGER,
+PartySize INTEGER)
 -- Hint: SQLite offers the INTEGER and REAL datatypes
