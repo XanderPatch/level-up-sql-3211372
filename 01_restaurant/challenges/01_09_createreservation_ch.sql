d21e22c75e453997d54676f736d7e1b4865d1eab
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 13:46:03 +1000

    Update 01_09_createreservation_ch.sql

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,19 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT * FROM Customers where Email = 'smac@kinetecoinc.com';
+--Customer does not exist, will add to customers table
+
+INSERT INTO Customers
+(FirstName, LastName, Email)
+VALUES('Sam', 'McAdams','smac@kinetecoinc.com');
+
+INSERT INTO Reservations (CustomerID, Date, PartySize)
+VALUES(
+(SELECT CustomerID FROM Customers WHERE Email = 'smac@kinetecoinc.com'),
+'2022-08-12 18:00:00',
+5
+);
+
+SELECT * FROM Reservations ORDER BY ReservationID DESC;
