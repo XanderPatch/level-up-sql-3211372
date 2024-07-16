c571f5af30578efe0fbe957ac3014b5fd6f36eba
XanderPatch <32567422+XanderPatch@users.noreply.github.com>
2024-07-16 14:27:25 +1000

    Update 01_12_bestcustomers_ch.sql

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,9 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT FirstName, LastName, Email, Count(Orders.OrderID) AS Orders_Placed FROM Orders
+JOIN Customers on Customers.CustomerID = Orders.CustomerID 
+GROUP BY Orders.CustomerID 
+ORDER BY Orders_Placed DESC
+LIMIT 6; --NATURAL BREAK AT 15 OR MORE ORDERS
